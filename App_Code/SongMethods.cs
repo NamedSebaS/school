fd4d1fa959f8f0d9a313b641d272d7ba27a8ea1f
nsonvt <nsonvtnguyen@gmail.com>
2021-05-21 09:27:25 +0200

    verbeteringen

--- App_Code/SongMethods.cs
+++ App_Code/SongMethods.cs
@@ -1,41 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Data;
 
 namespace school.App_Code
 {
-    public class Class1
-    {
-    }
+	public class SongMethods
+	{
+
+
+		public DataSet GetAllSongs(string file)
+		{
+			DataSet ds = new DataSet("playlist");
+
+			DataTable dtSongs = new DataTable("song");
+
+			DataColumn dcId = new DataColumn("id");
+			DataColumn dcArtist = new DataColumn("artist");
+			DataColumn dcTitle = new DataColumn("title");
+			DataColumn dcYear = new DataColumn("year");
+			DataColumn dcGenre = new DataColumn("genre");
+			DataColumn dcTime = new DataColumn("time");
+			DataColumn dcFile = new DataColumn("file");
+
+			dtSongs.Columns.Add(dcId);
+			dtSongs.Columns.Add(dcArtist);
+			dtSongs.Columns.Add(dcTitle);
+			dtSongs.Columns.Add(dcYear);
+			dtSongs.Columns.Add(dcGenre);
+			dtSongs.Columns.Add(dcTime);
+			dtSongs.Columns.Add(dcFile);
+			ds.Tables.Add(dtSongs);
+
+			ds.ReadXml(HttpContext.Current.Server.MapPath(file));
+
+			return ds;
+
+		}
+	}
+
 }
-public DataSet GetAllSongs(string file)
-{
-	DataSet ds = new DataSet("playlist");
-
-	DataTable dtSongs = new DataTable("song");
-
-	DataColumn dcId = DataColumn("id");
-	DataColumn dcArtist = new DataColumn("artist");
-	DataColumn dcTitle = new DataColumn("title");
-	DataColumn dcYear = new DataColumn("year");
-	DataColumn dcGenre = new DataColumn("genre");
-	DataColumn dcTime = new DataColumn("time");
-	DataColumn dcFile = new DataColumn("file");
-
-	dtSongs.Columns.Add(dcId);
-	dtSongs.Columns.Add(dcArtist);
-	dtSongs.Columns.Add(dcTitle);
-	dtSongs.Columns.Add(dcYear);
-	dtSongs.Columns.Add(dcGenre);
-	dtSongs.Columns.Add(dcTime);
-	dtSongs.Columns.Add(dcFile);
-	ds.Tables.Add(dtSongs);
-
-	ds.ReadXml(HttpContext.Current.Server.MapPath(file));
-
-	return ds;
-
-}
-
